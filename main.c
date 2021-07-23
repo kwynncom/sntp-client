5f53f7acce08c419e7f163ede116c17bc5c2e6ae
kwynncom <buessresume@gmail.com>
2021-07-22 22:32:19 -0400

    tweaks

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 #include <unistd.h> // read, write, close
 #include <stdio.h>  // stdout
 #include <stdlib.h> // malloc
-#include "config.h"
+
 #include "utils.h"
 
 void main(int argc, char **argv) {
@@ -11,7 +11,7 @@ void main(int argc, char **argv) {
 
 	char pack[snpl];
 	unsigned long b, e;
-	int sock = getOutboundUDPSock(getAddr(argc, argv), 123);
+	const int sock = getOutboundUDPSock(getAddr(argc, argv), 123);
 
     memcpy(pack    , "#",  1); // SNTP packet header - see readme
 	bzero (pack + 1,      47);
@@ -20,10 +20,10 @@ void main(int argc, char **argv) {
 	if (write(sock, pack, snpl) != snpl) perror("bad write");
     if (read (sock, pack, snpl) != snpl) perror("bad read");
 	e = nanotime();
+	close(sock);
 
-	fwrite(&pack, snpl, 1, stdout);
+	fwrite(&pack, snpl     , 1, stdout);
 	fwrite(&b	, sizeof(b), 1, stdout);
 	fwrite(&e	, sizeof(e), 1, stdout);
 
-	close(sock);
 }
