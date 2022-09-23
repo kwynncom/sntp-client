51cf3f6b90363c43beb57580688b894c581383c6
kwynncom <buessresume@gmail.com>
2022-09-23 02:24:53 -0400

    breaking down functions

--- call.c
+++ call.c
@@ -5,33 +5,42 @@
 #include "utils.h"
 
 void call20(int sock, struct timespec *bs, struct timespec *es, char *pack);
+void call30(const struct timespec bs, const struct timespec es, char *pack);
 
 void call10(int sock) {
 
     char pack[SNPL];
-    unsigned long b, e;
     struct timespec bsts;
     struct timespec ests;
     
     setOBPack(pack);
 
     call20(sock, &bsts, &ests, pack);
+    call30(       bsts,  ests, pack);
 
-    b = bsts.tv_sec * M_BILLION + bsts.tv_nsec;
-    e = ests.tv_sec * M_BILLION + ests.tv_nsec;
 
-    fwrite(&pack, SNPL     , 1, stdout);
-    fwrite(&b	, sizeof(b), 1, stdout);
-    fwrite(&e	, sizeof(e), 1, stdout); 
-    fflush(stdout);
+
+
 }
 
-void call20(int sock, struct timespec *bs, struct timespec *es, char *pack) {
+void call20(const int sock, struct timespec *bs, struct timespec *es, char *pack) {
 
     ssize_t ignoreForWarn;
 
     clock_gettime(CLOCK_REALTIME, bs);
-    ignoreForWarn = write(sock, pack, SNPL);
-    ignoreForWarn = read (sock, pack, SNPL);
+    if (write(sock, pack, SNPL) != SNPL) perror("bad write");
+    if (read (sock, pack, SNPL) != SNPL) perror("bad read");
     clock_gettime(CLOCK_REALTIME, es);
+}
+
+void call30(const struct timespec bs, const struct timespec es, char *pack) {
+    
+    const unsigned long b = bs.tv_sec * M_BILLION + bs.tv_nsec;
+    const unsigned long e = es.tv_sec * M_BILLION + es.tv_nsec;  
+    static size_t lsz = sizeof(b);
+    
+    fwrite( pack, SNPL, 1, stdout);
+    fwrite(&b	, lsz , 1, stdout);
+    fwrite(&e	, lsz , 1, stdout);
+    fflush(stdout); 
 }
