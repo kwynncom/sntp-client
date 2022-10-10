d3f742ff63a43b109195266f14b6f316677bbf82
kwynncom <buessresume@gmail.com>
2022-10-10 06:03:29 -0400

    close to a solution

--- ntpc/w.c
+++ ntpc/w.c
@@ -23,10 +23,21 @@ void myread() {
 	const int sz = 214; // based on size of output right now
 	char b[sz + 1];
 	rd = open(KWSNTPDEXTGET, O_RDONLY | O_NONBLOCK);
-	sleep(2);
-	read(rd, b, sz);
+	
+	int i=0;
+	const int  stepsn  =  7;
+	const int steps[7]  = {2, 5, 5, 40, 50, 200, 700};
+
+
+	for (i=0; i < stepsn; i++) {
+		usleep(steps[i] * 1000);
+		read(rd, b, sz);
+		if (strstr(b, "**OK**") != NULL) break;
+		if (strstr(b, "fails" ) != NULL) break;
+	}
 	close(rd);
 	printf("%s\n", b);
+	printf("%d\n", i);
 }
 
 void main(void) { // I will need to account for the exit "x" rather than non-x
