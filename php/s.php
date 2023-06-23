1dd3a399fdd69d3ce647edec924ea937ab7b3d05
kwynncom <buessresume@gmail.com>
2023-06-23 19:37:42 -0400

    no longer uses /opt/kwynn sntp sanity test

--- php/s.php
+++ php/s.php
@@ -2,6 +2,11 @@
 
 require_once('/opt/kwynn/kwutils.php');
 
+// 2 classes:
+// 1. The sntp_wrapper that calls the C version
+// 1.5 - Not a class, but the call to the wrapper
+// 2. That version of the "sanity" class to check the result
+
 class sntp_wrapper {
 	
 	const cmdBB = 'sntpkw';
@@ -143,7 +148,7 @@ class sntp_wrapper {
 			if ($this->sane)
 				 echo('Results **OK**' . "\n");
 			else echo("sanity failures\n");
-			echo('Sanity VERSION: ' . sntpSanity::ssVersion . "\n");
+			echo('Sanity VERSION: ' . sntpSanityUns4::ssVersion . "\n");
 		}		
 
 		$this->jsonf();
@@ -199,7 +204,7 @@ class sntp_wrapper {
 		$this->sane = false;
 		$a = [];
 		try {
-			$ret = sntpSanity::ck($t, !$this->dojson);
+			$ret = sntpSanityUns4::ck($t, !$this->dojson);
 			if (!$ret) {
 				if (!$this->dojson) echo("**Fail\n");
 				exit(153);
@@ -218,3 +223,83 @@ class sntp_wrapper {
 }
 
 new sntp_wrapper();
+
+class sntpSanityUns4 {
+	
+	const tln   = 4;
+	const ipi   = self::tln;
+	const tolns = M_BILLION;
+	const ssVersion = '2023/01/20 19:26 - printing PHP hutime';
+	
+	public static function ck(string $t, bool $contiff = false) {
+		$o = new self($t, $contiff);
+		return $o->getRes();
+		
+	}
+	
+	private function __construct(string $t, bool $contiff) {
+		$this->contif  = $contiff;
+		$this->oret = false;
+		$this->sanFail = false;
+		$this->oret = $this->ttoa($t);
+	}
+	
+	public function getRes() {
+		return $this->oret;
+	}
+	
+	private function ass(bool $tock, string $msg, bool $failAnyhow = false) {
+		if ($tock) return true;
+		$this->sanFail = true;
+		if ($this->contif && !$failAnyhow) echo("**FAIL: " . $msg . "\n");
+		else throw new Exception($msg);
+		
+	}
+	
+	private function ttoa(string $t) {
+		
+		static $failv = [];
+
+		try {
+			
+			$o = $this;
+			
+			if (!$o->ass($t, 'blank string')) return $failv;
+			
+			$a  = explode("\n", trim($t));
+			$o->ass($a && is_array($a) && count($a) >= self::tln, 'wrong lines sntp ck - out: ' . "$t");  unset($t); 
+			$ip = getValidIPOrFalsey(kwifs($a, self::ipi)); 
+			$a = array_slice($a, 0, self::tln);
+
+			$n = self::tln;
+			$o->ass(count($a) === $n, 'bad tline count sntp sanity 2', true);
+			for ($i=0; $i < $n; $i++) {
+				if (is_numeric($a[$i])) 
+					 $a[$i] = intval($a[$i]);
+				else kwas(false, 'non-numeric value sent to time array 4 Uns');
+			}
+
+			$o->ass(count($a) >= 4, 'fail - for immediate tline sntp sanity purposes', true);
+
+			$min = min($a);
+			$max = max($a);
+			$o->ass($max - $min < self::tolns, 'time sanity check fails');
+			$nowns = nanotime();
+			$ds = abs($nowns - $max);
+			$o->ass($ds < self::tolns, 'time sanity check fail 2 - perhaps quota fail; now ns PHP = ' . date('H:i:s', roint($nowns / M_BILLION)));
+			$o->ass($a[1] <= $a[2], 'server time sanity check fail between in and out');
+			$o->ass($a[0] <  $a[3], 'server time sanity check internal out and in');
+
+			$ret['ip'  ] = $ip;
+			$ret['Uns4'] = $a;
+			$ret['sane'] = !$this->sanFail;
+
+			return $ret;
+		} catch (Exception $ex) {	}
+		
+		return $failv;
+	} // func
+	
+	public static function SNTPOffset($T) {	return ((($T[1] - $T[0]) + ($T[2] - $T[3]))) >> 1;	}
+
+} // class
